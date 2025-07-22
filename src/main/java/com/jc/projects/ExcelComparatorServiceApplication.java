69aa20513c3cb2dfa39fa155dab24c67b533db17
Jayashish <jayashish.chowdhury@gmail.com>
2025-07-22 19:44:34 +0530

    Initial Commit

---
+++ src/main/java/com/jc/projects/ExcelComparatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jc.projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelComparatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelComparatorServiceApplication.class, args);
+	}
+
+}
